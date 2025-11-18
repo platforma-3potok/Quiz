d193c13a61bb872bf6945fef3bfaf5a495ecb67e
Ищейкин Кирилл <yasuooo@MacBook-Air-Isejkin.local>
2025-11-18 22:48:16 +0300

    random_ord

---
+++ auxiliary_func.py
@@ -0,0 +1,10 @@
+import random
+
+def random_order(start: int = 1, end: int = 10, count: int = 10):
+    if not isinstance(start, int):
+        raise ValueError('id первого вопроса должен быть число')
+    if not isinstance(end, int):
+        raise ValueError('id последнего вопроса должен быть число')
+    if not isinstance(count, int):
+        raise ValueError('Кол-во вопросов должно быть числом')    
+    return random.sample(range(start, end+1), count)
