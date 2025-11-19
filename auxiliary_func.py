79782ece48c656576a6c43fbf4bef173804995a2
Ищейкин Кирилл <yasuooo@MacBook-Air-Isejkin.local>
2025-11-19 21:33:39 +0300

    quiz

--- auxiliary_func.py
+++ auxiliary_func.py
@@ -1,6 +1,6 @@
 import random
 
-def random_order(start: int = 1, end: int = 10, count: int = 10):
+def random_order(start: int = 0, end: int = 10, count: int = 10):
     if not isinstance(start, int):
         raise ValueError('id первого вопроса должен быть число')
     if not isinstance(end, int):
